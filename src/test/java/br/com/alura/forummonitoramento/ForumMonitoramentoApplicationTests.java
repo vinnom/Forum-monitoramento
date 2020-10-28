d38f63a845c7cd67d70699337a927495aff3b8fe
vinnom <vinnom.2d@gmail.com>
2020-10-28 01:38:53 -0300

    commit inicial

---
+++ src/test/java/br/com/alura/forummonitoramento/ForumMonitoramentoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.alura.forummonitoramento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForumMonitoramentoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
